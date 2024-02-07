2fbb8cca504f3f84a5096620849211fd42f2655b
pburyanov <p_buryanov-itssv@kw.com>
2024-02-07 02:10:03 +0200

    : upd

--- init.lua
+++ init.lua
@@ -252,7 +252,9 @@ do
       --component_separators = { left = '╲', right = '╱'},
       --section_separators   = { left = '', right = ''},
       component_separators = { left = '╲╲', right = '╲╲'},
-      section_separators   = { left = " ", right = " "},
+      section_separators   = { left = "", right = ""},
+      --component_separators = { left = "░", right = "░" },
+      --section_separators   = { left = "▓▒░", right = "░▒▓" },
     },
     sections = {
       lualine_a = { "mode", "location" },
